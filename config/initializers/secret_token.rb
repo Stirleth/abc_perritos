2f31f035f8cc5df17b0c1455c9ed103ce489f5d2
Ale Navarro <la_reformita@hotmail.com>
2014-11-12 13:37:46 -0700

    Inicializando el proyecto abc_perritos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbcPerritos::Application.config.secret_token = '381b9b52fce7e399eb707323d14ec04a880e7ad8adde49ea7fd18fc0621651756e909a7ca7e030bf0a37ff5dae851c34a1320e7b8aa95b4113811b9ee51fe3c6'
